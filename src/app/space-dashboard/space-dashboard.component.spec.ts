d4234e2f65d49d93c5c15045bb6685dd8199b03c
Deepak D Nair <deepak.nair@OPN-MC-070.local>
2020-12-01 18:14:30 +0530

    update

--- src/app/space-dashboard/space-dashboard.component.spec.ts
+++ src/app/space-dashboard/space-dashboard.component.spec.ts
@@ -1,6 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, of } from 'rxjs';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 
 import { SpaceDashboardComponent } from './space-dashboard.component';
+
+export class MockActivatedRoute {
+
+  params: { [key: string]: any } = {
+    launch_year: '2020',
+    launch_success: 'true',
+    land_success: 'true'
+  };
+
+  constructor() {
+  }
+
+  get queryParams(): Observable<any> {
+    return of(this.params);
+  }
+
+}
 
 describe('SpaceDashboardComponent', () => {
   let component: SpaceDashboardComponent;
@@ -8,7 +30,14 @@ describe('SpaceDashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SpaceDashboardComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      declarations: [ SpaceDashboardComponent ],
+      providers: [
+      //  {provide: ActivatedRoute, useClass: MockActivatedRoute},
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +50,20 @@ describe('SpaceDashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should ngOnInit exist for SpaceDashboardComponent', () => {
+    expect(component.ngOnInit());
+  });
+  it('should call filterByYear', () => {
+    expect(component.filterByYear('2020'));
+    expect(component.params.launch_year).toBe('2020');
+  });
+  it('should call filterByLaunching', () => {
+    expect(component.filterByLaunching('true'));
+    expect(component.params.launch_success).toBe('true');
+  });
+  it('should call filterByYear', () => {
+    expect(component.filterByLanding('false'));
+    expect(component.params.land_success).toBe('false');
   });
 });
